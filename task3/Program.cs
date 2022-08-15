c844bc2ecd3f9e0ded45e88e3e48f862915e120b
Aleksey <smit_alexey@mail.ru>
2022-08-16 02:42:53 +0500

    Задали массив

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размерность массивов (от 2 до 100)  ");
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = new Random().Next(-100, 100);
+            Console.WriteLine("a[{0}] = {1}", i, a[i]);
+        }
+            Console.ReadKey();
+    }
+}
