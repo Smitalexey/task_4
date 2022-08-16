86094bfe5afa53f3f2c312dc9a15c4c55d14bd37
Aleksey <smit_alexey@mail.ru>
2022-08-16 07:42:44 +0500

    Число а в степени b

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите два целых числа ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Число а в степени b равно ");
+        Console.WriteLine(Math.Pow(a,b));
+
+    }
+}
