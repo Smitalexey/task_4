c62a6f8ad8eef3f303b176a48b23dc868b6d7802
Aleksey <smit_alexey@mail.ru>
2022-08-18 05:27:07 +0500

    Сумма цифр в числе

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите любое целое число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int SumNumber(int number)
+        {
+            int count = Convert.ToString(number).Length;
+            int newCount = 0;
+            int result = 0;
+            for (int i = 0; i < count; i++)
+            {
+                newCount = number - number%10;
+                result = result + (number - newCount);
+                number = number/10;
+            }
+            return result;
+        }
+            int sumNumber = SumNumber(number);
+            Console.WriteLine("Сумма цифр в числе = " + sumNumber);
+    }
+}
